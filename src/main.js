508dadb05b8b8d9743f1f13cde91bee8faf5f95c
Moran1324 <moran1324@gmail.com>
2020-07-19 17:19:15 +0300

    add button finished

--- src/main.js
+++ src/main.js
@@ -1,3 +1,45 @@
 const itemList = document.getElementById('list');
-let textInput = document.getElementById('textInput');
+const textInput = document.getElementById('textInput');
 const addButton = document.getElementById('addButton');
+const todoList = document.getElementById('todoList');
+const prioritySelector = document.getElementById('prioritySelector');
+textInput.focus();
+//what happens when the add button is clicked:
+addButton.addEventListener('click', addToList);
+function addToList() {
+    // task container
+    let taskContainer = document.createElement('div');
+    taskContainer.className = 'todoContainer';
+    todoList.appendChild(taskContainer);
+
+    // task priority
+    let taskPriority = document.createElement('span');
+    taskPriority.className = 'todoPriority';
+    taskPriority.innerHTML = prioritySelector.value;
+    taskContainer.appendChild(taskPriority);
+    
+    // task time stamp
+    let timeStamp = document.createElement('span');
+    let today = new Date();
+    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    let dateNtime = date+' '+time;
+    timeStamp.className = 'todoCreatedAt';
+    timeStamp.innerHTML = dateNtime;
+    taskContainer.appendChild(timeStamp);
+
+    // task type...
+
+    // task name
+    let task = textInput.value;
+    let addedTask = document.createElement('span');
+    addedTask.className = 'todoText';
+    addedTask.innerHTML = task;
+    taskContainer.appendChild(addedTask);
+
+    // task checkbox...
+
+
+    textInput.value = '';
+
+}
